80e7e4ebac9b87678e131861095d768c139770d9
Anna Mefford <annamefford@gmail.com>
2016-11-09 17:06:19 -0700

    cleaned up /pets

--- expressServer.js
+++ expressServer.js
@@ -9,28 +9,10 @@ var petsPath = path.join(__dirname, 'pets.json');
 console.log(petsPath);
 
 app.get("/pets", function(req, res){
-//res.send("HELLO THERE" + petsPath);
 
   fs.readFile(petsPath, 'utf8', function(err, data) {
-
-    if (err) {
-      throw err;
-    }
-
-    var index = process.argv[3];
     var pets = JSON.parse(data);
-
-    if (index !== undefined){
-      res.send((pets[index]));
-    }
-    if (index === undefined){
-      res.send((pets));
-    }
-    else if (index > pets.length || pets.length > 3){
-        res.send((`ERROR`));
-        process.exit(1);
-      }
-
+    res.send(pets);
   });
 });
 
