6b8f9c3c24bf7de02c0b007d249319a263416d73
Anna Mefford <annamefford@gmail.com>
2016-11-10 16:39:09 -0700

    post request working

--- expressServer.js
+++ expressServer.js
@@ -43,16 +43,40 @@ app.get("/pets/-1", function(req, res){
   });
 });
 
+app.get("/pets/:index", function(req, res){
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+    var pets = JSON.parse(data);
+    var index = Number.parseInt(req.params.index);
+    if (index > pets.length || index < 0){
+      res.sendStatus(404);
+    }
+  });
+});
+
 app.post("/pets", function(req, res){
-  var name=req.body.name;
-  var age=req.body.age;
-  var type=req.body.type;
-  console.log(name, age, type);
-  res.send(req.body);
+    var name=req.body.name;
+    var age=Number.parseInt(req.body.age);
+    var kind=req.body.kind;
+
+    if (Number.isNaN(age) || name === undefined || kind === undefined || age === undefined){
+      return res.sendStatus(400);
+    }
+    fs.readFile(petsPath, 'utf8', function(err, data) {
+      var pets = JSON.parse(data);
+      pets.push(req.body);
+      var petsJSON = JSON.stringify(pets);
+
+      fs.writeFile(petsPath, petsJSON, function(writeErr){
+        if (writeErr) {
+          throw writeErr;
+        }
+      });
+    });
+    res.send(req.body);
 });
 
 app.listen('3000', function(){
-  //console.log('listening on port 3000');
+  console.log('listening on port 3000');
 });
 
 module.exports = app;
