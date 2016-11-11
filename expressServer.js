2eb7c6c55430100470440201b04035470bddd88e
Anna Mefford <annamefford@gmail.com>
2016-11-10 17:01:20 -0700

    updating index search

--- expressServer.js
+++ expressServer.js
@@ -18,38 +18,40 @@ app.get("/pets", function(req, res){
     res.send(pets);
   });
 });
-app.get("/pets/0", function(req, res){
-  fs.readFile(petsPath, 'utf8', function(err, data) {
-    var pets = JSON.parse(data);
-    res.send(pets[0]);
-  });
-});
-app.get("/pets/1", function(req, res){
-  fs.readFile(petsPath, 'utf8', function(err, data) {
-    var pets = JSON.parse(data);
-    res.send(pets[1]);
-  });
-});
-
-app.get("/pets/2", function(req, res){
-  fs.readFile(petsPath, 'utf8', function() {
-    res.sendStatus(404);
-  });
-});
-
-app.get("/pets/-1", function(req, res){
-  fs.readFile(petsPath, 'utf8', function() {
-    res.sendStatus(404);
-  });
-});
+// app.get("/pets/0", function(req, res){
+//   fs.readFile(petsPath, 'utf8', function(err, data) {
+//     var pets = JSON.parse(data);
+//     res.send(pets[0]);
+//   });
+// });
+// app.get("/pets/1", function(req, res){
+//   fs.readFile(petsPath, 'utf8', function(err, data) {
+//     var pets = JSON.parse(data);
+//     res.send(pets[1]);
+//   });
+// });
+//
+// app.get("/pets/2", function(req, res){
+//   fs.readFile(petsPath, 'utf8', function() {
+//     res.sendStatus(404);
+//   });
+// });
+//
+// app.get("/pets/-1", function(req, res){
+//   fs.readFile(petsPath, 'utf8', function() {
+//     res.sendStatus(404);
+//   });
+// });
 
 app.get("/pets/:index", function(req, res){
-  fs.readFile(petsPath, 'utf8', function(err, data) {
+  var index = Number.parseInt(req.params.index);
+  fs.readFile(petsPath, 'utf8', index, function(err, data) {
     var pets = JSON.parse(data);
-    var index = Number.parseInt(req.params.index);
+    console.log(pets[index]);
     if (index > pets.length || index < 0){
-      res.sendStatus(404);
+       return res.sendStatus(404);
     }
+    res.send(pets[index]);
   });
 });
 
